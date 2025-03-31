e2819c36cf50de72baa712862dc46d8e553e638c
SauloMiller <saulomiller1215@hotmail.com>
2025-03-31 16:41:47 -0300

    Chamar os métodos de uma classe .NET

--- freeCodecamp/CallMethodsFromDotNetClassLibrary.cs
+++ freeCodecamp/CallMethodsFromDotNetClassLibrary.cs
@@ -1,10 +1,20 @@
 
-using System;
-public static class CallMethodsFromDotNetClassLibrary
-{
-    public static void Executar()
-    {
-        Console.WriteLine("Executar CallMethodsFromDotNetClassLibrary");
-    }
-}
+// using System;
+// public static class CallMethodsFromDotNetClassLibrary
+// {
+//     public static void Executar()
+//     {
+//         Console.WriteLine("Executar CallMethodsFromDotNetClassLibrary");
+//     }
+// }
 
+// Random dice = new Random();
+// int roll = dice.Next(1,7);
+// Console.WriteLine(roll);
+
+
+//sem a criacao de metodo 
+//int result =Random.Next();
+
+Random dice = new Random();
+int roll = dice.Next();
