5c4300eb0b3f06096459b3830f164ebf283e26d9
SauloMiller <saulomiller1215@hotmail.com>
2025-03-31 17:29:01 -0300

    Métodos sobrecarregados

--- freeCodecamp/CallMethodsFromDotNetClassLibrary.cs
+++ freeCodecamp/CallMethodsFromDotNetClassLibrary.cs
@@ -8,6 +8,8 @@
 //     }
 // }
 
+////////Exercício – Chamar os métodos de uma classe .NET//////
+
 // Random dice = new Random();
 // int roll = dice.Next(1,7);
 // Console.WriteLine(roll);
@@ -16,5 +18,19 @@
 //sem a criacao de metodo 
 //int result =Random.Next();
 
+// Random dice = new Random();
+// int roll = dice.Next();
+
 Random dice = new Random();
-int roll = dice.Next();
+
+int roll1 = dice.Next(-100,100);
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+
+Console.WriteLine($"Second roll: {roll2}");
+
+Console.WriteLine($"Third roll: {roll3}");
+
+
