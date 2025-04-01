e0137a1bdf2f78b51bdee5df511ad679dc82fa62
SauloMiller <saulomiller1215@hotmail.com>
2025-04-01 18:18:38 -0300

    modulo finalizado

--- freeCodecamp/CallMethodsFromDotNetClassLibrary.cs
+++ freeCodecamp/CallMethodsFromDotNetClassLibrary.cs
@@ -8,31 +8,38 @@
 //     }
 // }
 
-////////Exercício – Chamar os métodos de uma classe .NET//////
+// //////Exercício – Chamar os métodos de uma classe .NET//////
 
 // Random dice = new Random();
 // int roll = dice.Next(1,7);
 // Console.WriteLine(roll);
 
 
-//sem a criacao de metodo 
-//int result =Random.Next();
+// sem a criacao de metodo 
+// int result =Random.Next();
 
 // Random dice = new Random();
 // int roll = dice.Next();
 
-Random dice = new Random();
+// Random dice = new Random();
 
-int roll1 = dice.Next(-100,100);
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+// int roll1 = dice.Next(-100,100);
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
 
-Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"First roll: {roll1}");
 
-Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Second roll: {roll2}");
 
-Console.WriteLine($"Third roll: {roll3}");
+// Console.WriteLine($"Third roll: {roll3}");
 
-dice.Next();
+// dice.Next();
 
 
+
+int firstValue = 700;
+int secondValue = 600;
+
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
